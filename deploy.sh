308af54ce26c5a65d3a70fa4f7532ab7de99e7cf
Pudding <>
2022-03-05 17:50:31 +0800

    build: 修改自動部署文件

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,16 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# 把 Github 主分支改為 master，或是更改 git init 預設分支名稱
+
+# 修改本地 git init 預設分支為 master 
+# git config --global init.defaultBranch master
+
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f https://github.com/<USERNAME>/<USERNAME>.GitHub.io.git main
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:jj280385/Twitter-by-ALPHA-Camp.git main:gh-pages
+git push -f https://github.com/Pudding1989/Twitter-by-ALPHA-Camp.git master:gh-pages
 
 cd -
